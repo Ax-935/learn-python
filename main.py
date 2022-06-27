791b78653a639335ae63563cbf44865650ca9aa8
alex mirzaee <>
2022-06-27 08:02:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+#Write your code below this line 👇
+print( len(input("what is you'r name ?")))
+print( len(input("what is you'r last name ?")))
+print( len(input("what is you'r full name(without spaces) ?")))
